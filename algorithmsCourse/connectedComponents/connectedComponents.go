8dbfc0d25bac9b9739f4498b91fdea7534c50281
Alexandru <alexandrutodea1992@gmail.com>
2021-12-23 15:28:49 +0000

    finish implementation of kosaraju algorithm

--- algorithmsCourse/connectedComponents/connectedComponents.go
+++ algorithmsCourse/connectedComponents/connectedComponents.go
@@ -12,81 +12,107 @@ import (
 var inputFilePath = "/Users/alexandrutodea/go/src/leetcode-algorithms/algorithmsCourse/connectedComponents/graph_week_1_prog_as.txt"
 
 type Graph struct {
-	topFiveConnectedComponents []int
-	vertices                   map[int]*Vertex
+	vertices          []*Vertex
+	verticesPositions []int
+	sccs              []int
 }
 
 type Vertex struct {
-	label    int
 	visited  bool
+	incoming []int
 	outgoing []int
+}
+
+func (g *Graph) getPositions() {
+	for currentVertexIndex := len(g.vertices) - 1; currentVertexIndex > 0; currentVertexIndex-- {
+		if g.vertices[currentVertexIndex].visited {
+			continue
+		}
+		g.dfsForFindingVertexPosition(currentVertexIndex)
+	}
+}
+
+func (g *Graph) dfsForFindingVertexPosition(currentVertexIndex int) {
+	g.vertices[currentVertexIndex].visited = true
+	for _, incomingVertexIndex := range g.vertices[currentVertexIndex].incoming {
+		if g.vertices[incomingVertexIndex].visited {
+			continue
+		}
+		g.dfsForFindingVertexPosition(incomingVertexIndex)
+	}
+
+	g.verticesPositions = append(g.verticesPositions, currentVertexIndex)
+}
+
+func (g *Graph) dfsForFindingSCCs(currentVertexIndex int) int {
+	g.vertices[currentVertexIndex].visited = true
+	numberVerticesInCluster := 1
+	for _, outgoingVertex := range g.vertices[currentVertexIndex].outgoing {
+		if g.vertices[outgoingVertex].visited {
+			continue
+		}
+		numberVerticesInCluster += g.dfsForFindingSCCs(outgoingVertex)
+	}
+
+	return numberVerticesInCluster
 }
 
 func main() []int {
 	data, err := parseFile(inputFilePath)
+	graph := generateGraph(data, 875715) // for actual input
+	// graph := generateGraph(data, 10) // for test2 file
+	// graph := generateGraph(data, 9) // for test1 and test3 and test5 files
+	// graph := generateGraph(data, 13) // for test4
+	fmt.Print(graph)
 
 	if err != nil {
 		panic("error parsing the file")
 	}
 
-	graph := Graph{topFiveConnectedComponents: make([]int, 5), vertices: map[int]*Vertex{}}
-	graph.construct(data)
-	return graph.findTopFiveConnectedComponents()
+	graph.getPositions()
+	graph.resetVisited()
+	graph.getConnectedComponents()
+	sort.Ints(graph.sccs)
+
+	return graph.sccs
 }
 
-func (g *Graph) findTopFiveConnectedComponents() []int {
-	output := []int{}
-	for label, vertex := range g.vertices {
-		if vertex.visited {
+func (g *Graph) getConnectedComponents() {
+	for i := len(g.verticesPositions) - 1; i >= 0; i-- {
+		vertexIndex := g.verticesPositions[i]
+		if g.vertices[vertexIndex].visited {
 			continue
 		}
-
-		output = append(output, g.bfs(label, 0))
-	}
-
-	sort.Ints(output)
-	return output[:5]
-}
-
-func (g *Graph) bfs(label int, nrComponents int) int {
-	if g.vertices[label].visited || len(g.vertices[label].outgoing) == 0 {
-		return nrComponents
-	}
-
-	g.vertices[label].visited = true
-	totalComponents := nrComponents + 1
-
-	for _, label := range g.vertices[label].outgoing {
-		totalComponents += g.bfs(label, totalComponents)
-	}
-
-	return totalComponents
-}
-
-func (g *Graph) construct(inputData [][]int) {
-	for _, vertices := range inputData {
-		sourceVertexAddress := g.addVertex(vertices[0])
-		destinationVertexAddress := g.addVertex(vertices[1])
-		g.addEge(sourceVertexAddress, destinationVertexAddress)
+		g.sccs = append(g.sccs, g.dfsForFindingSCCs(vertexIndex))
 	}
 }
 
-func (g *Graph) addVertex(label int) int {
-	_, vertexExists := g.vertices[label]
-
-	if !vertexExists {
-		g.vertices[label] = &Vertex{label: label, outgoing: []int{}, visited: false}
+func (g *Graph) resetVisited() {
+	for i := 1; i < len(g.vertices); i++ {
+		g.vertices[i].visited = false
 	}
-
-	return label
 }
 
-func (g *Graph) addEge(sourceLabel int, destinationLabel int) {
-	if sourceLabel == destinationLabel {
-		return
+func generateGraph(input [][]int, numberVertices int) Graph {
+	graph := Graph{vertices: make([]*Vertex, numberVertices), verticesPositions: []int{}, sccs: []int{}}
+
+	for _, subArr := range input {
+		from := subArr[0]
+		to := subArr[1]
+
+		if graph.vertices[from] == nil {
+			graph.vertices[from] = &Vertex{}
+		}
+
+		if graph.vertices[to] == nil {
+			graph.vertices[to] = &Vertex{}
+		}
+
+		graph.vertices[from].outgoing = append(graph.vertices[from].outgoing, to)
+		graph.vertices[to].incoming = append(graph.vertices[to].incoming, from)
 	}
 
-	g.vertices[sourceLabel].outgoing = append(g.vertices[sourceLabel].outgoing, destinationLabel)
+	return graph
 }
 
 func parseFile(filePath string) ([][]int, error) {
