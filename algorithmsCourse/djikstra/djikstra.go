abeebd222ba29ace8a21a739d45124ba357dca4b
Alexandru <alexandrutodea1992@gmail.com>
2022-03-05 12:37:51 +0200

    create graph from file

--- algorithmsCourse/djikstra/djikstra.go
+++ algorithmsCourse/djikstra/djikstra.go
@@ -8,41 +8,64 @@ import (
 	"strings"
 )
 
-// type Edge struct {
-// 	to    int
-// 	score int
-// }
-
-// type Vertex []*Edge
-
-// type Graph struct {
-// 	vertices []*Vertex
-// }
-
-// func (g *Graph) init() {}
-
-func main() {
-	fileName := "/datasetMain.txt"
-	pwd, _ := os.Getwd()
-	filePath := pwd + fileName
-	data, err := parseFile(filePath)
-
-	if err != nil {
-		panic("error parsing the file")
-	}
-
-	fmt.Print(data)
+type Graph struct {
+	adjacencyList []*Edges
 }
 
-// todo error parsing file
-func parseFile(filePath string) ([][]int, error) {
+type Edges []*Edge
+
+type Edge struct {
+	to    int
+	score int
+}
+
+func (edges *Edges) addEdgesFromBatch(indexInEdgesArray int, batch []string) error {
+	if len(batch) == 0 {
+		return nil
+	}
+
+	batchHead := batch[0]
+	splittedStrings := strings.Split(batchHead, ",")
+	to, err := strconv.Atoi(splittedStrings[0])
+
+	if err != nil {
+		fmt.Print("Cannot convert " + splittedStrings[0] + " to int")
+		return err
+	}
+
+	score, err := strconv.Atoi(splittedStrings[1])
+
+	if err != nil {
+		fmt.Print("Cannot convert " + splittedStrings[1] + " to int")
+		return err
+	}
+
+	edge := Edge{
+		to:    to,
+		score: score,
+	}
+
+	(*edges)[indexInEdgesArray] = &edge // add edge at location 'indexInEdgesArray' within edges arr. this is happening because we pre-assigned memory for this array
+
+	nextIndex := indexInEdgesArray + 1
+	batchTail := batch[1:]
+
+	return edges.addEdgesFromBatch(nextIndex, batchTail)
+}
+
+func initGraph() Graph {
+	return Graph{adjacencyList: []*Edges{}}
+}
+
+func (g *Graph) createGraphFromFile(filePath string) error {
+
 	fmt.Println("Parsing file...")
-	var fileRows [][]int
+	g.adjacencyList = append(g.adjacencyList, &Edges{}) // append dummy vertex at index 0 in order to maintain correct naming
 
 	file, err := os.Open(filePath)
 
 	if err != nil {
-		return [][]int{}, err
+		return err
 	}
 
 	defer file.Close()
@@ -51,29 +74,34 @@ func parseFile(filePath string) ([][]int, error) {
 
 	for scanner.Scan() {
 		lineAsString := scanner.Text()
-		row := strings.Fields(lineAsString)
-		rowNew := make([]int, len(row))
-
-		for i, valAsString := range row {
-			valAsInt, err := strconv.Atoi(valAsString)
-
-			if err != nil {
-				return [][]int{}, err
-			}
-
-			rowNew[i] = valAsInt
-		}
-
-		fileRows = append(fileRows, rowNew)
+		rowArrayOfStrings := strings.Fields(lineAsString)
+		thisVertexEdges := make(Edges, len(rowArrayOfStrings[1:]))
+		err := thisVertexEdges.addEdgesFromBatch(0, rowArrayOfStrings[1:]) // skip first element because that's the vertex
 
 		if err != nil {
-			return [][]int{}, err
+			return err
 		}
+
+		g.adjacencyList = append(g.adjacencyList, &thisVertexEdges)
 	}
 
 	if err := scanner.Err(); err != nil {
-		return [][]int{}, err
+		return err
 	}
-	fmt.Println("File parsed succesfully")
-	return fileRows, nil
+
+	fmt.Println("Graph created succesfully")
+	return nil
+}
+
+func main() {
+	fileName := "/datasetMain.txt"
+	pwd, _ := os.Getwd()
+	filePath := pwd + fileName
+	graph := initGraph()
+
+	err := graph.createGraphFromFile(filePath)
+
+	if err != nil {
+		panic("error creating graph from file")
+	}
 }
